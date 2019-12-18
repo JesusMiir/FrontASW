febbdeb56cbbb52479ef539985fb6b61974d426c
Jesús Mercadal Mir <jesusmercadalmir@gmail.com>
2019-12-18 20:03:57 +0100

    casi acabat

--- src/app/components/edit-issue/edit-issue.component.ts
+++ src/app/components/edit-issue/edit-issue.component.ts
@@ -2,8 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { UserService } from '../../services/user.services';
 import { Issue } from 'src/app/models/issue';
+import { Comment } from 'src/app/models/comment';
 import { UploadedFile } from 'src/app/models/uploadedFile';
 import { IssueService } from 'src/app/services/issue.services';
+import { User } from 'src/app/models/user';
+import { UserAssignee } from 'src/app/models/userAssignee';
 
 @Component({
   selector: 'app-edit-issue',
@@ -15,8 +18,12 @@ export class EditIssueComponent implements OnInit {
 
   public id;
   public user;
+  public userAssignee;
   public identity;
+  public comment;
   public token;
+  public users;
+  public idAssignee;
   public issue: Issue;
   public uploadedFile: UploadedFile;
 
@@ -27,8 +34,13 @@ export class EditIssueComponent implements OnInit {
     private _issueService: IssueService
   ) { 
     this.id = this._route.snapshot.paramMap.get('id');
+    this.token = this._userService.getToken();
     this.issue = new Issue(null, null, null, '', '', 'NEW', 'BUG', 'MAJOR', 0, null, null, 0);
+    this.comment = new Comment(null, null, null, '');
     this.uploadedFile = new UploadedFile(null, null, '', null);
+    this.identity = this._userService.getIdentity();
+    this.users = [];
+    this.userAssignee = null; 
   }
 
   ngOnInit() {
@@ -38,21 +50,35 @@ export class EditIssueComponent implements OnInit {
   }
 
   redirectIfIdentity() {
-    let identity = this._userService.getIdentity();
-    if (identity == null) {
+    if (this.identity == null) {
       this._router.navigate(["/login"]);
     }
   }
 
   onSubmit() {
-    this.issue.creationDate = new Date();
     console.log(this.issue);
-    this._issueService.create(this.issue, this.token).subscribe(
+    
+    var newIssue;
+    newIssue.title = this.issue.title;
+    newIssue.description = this.issue.description;
+    newIssue.type = this.issue.type;
+    newIssue.priority = this.issue.priority;
+    if (this.userAssignee != null || this.userAssignee != '')
+      newIssue.userAssigneeId = this.userAssignee;
+    console.log(newIssue);
+    
+    
+    this._issueService.edit(this.id, newIssue, this.token).subscribe(
       response => {
         console.log(response);
-        this._router.navigate(["/issue/"+response.id]);
+        this._router.navigate(["/issue/"+this.id]);
       }
     );
+    
+  }
+
+  onChange() {
+    console.log(this.userAssignee);
   }
 
   getIssue() {
@@ -62,6 +88,19 @@ export class EditIssueComponent implements OnInit {
         this.user = response.userCreator;
         this.issue.userAssigneeId = response.userAssignee;
         console.log(this.issue);
+        this.getUsers();
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  getUsers() {
+    this._userService.getUsers(this.token).subscribe(
+      response => {
+        console.log(response);
+        this.users = response;
       },
       error => {
         console.log(error);
